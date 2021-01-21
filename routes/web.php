9fc85ce82fa00a5a3a900a80ca6b572d1c3d6f2b
Elanganathapillai Sivakanthan <siva@globalcitymedia.com>
2021-01-21 19:53:42 +0000

    Add new menus

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = "welcome";
+    return view('welcome',compact('title'));
+})->name('home');
+
+
+Route::get('/about', function () {
+    $title = "about";
+    return view('welcome',compact('title'));
+})->name('about');
+
+
+
+Route::get('/contact', function () {
+    $title = "contact";
+    return view('welcome',compact('title'));
+})->name('contact');
+
+
